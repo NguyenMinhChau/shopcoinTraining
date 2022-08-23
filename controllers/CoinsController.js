8258cea02b11125d9293454fc65b58e0f771e397
hoainam2001 <nammaiyeutran@gmail.com>
2022-08-23 12:03:48 +0700

    v1.0.6

--- controllers/CoinsController.js
+++ controllers/CoinsController.js
@@ -3,17 +3,28 @@ const Coins = require('../models/Coins')
 class CoinsController{
     // [POST] /coins/add
     addCoin(req, res){
-        const {logo, name, symbols} = req.body
-        const coin = Coins({
-            logo: logo,
-            coinName: name,
-            symbols: symbols,
-        })
-        coin.save()
-        .then(coin => {
-            return res.json({code: 1, coin: coin})
-        })
-        .catch(err => res.json({code: 2, message: err.message}))
+        // const {logo, name, symbols} = req.body
+        // const coin = Coins({
+        //     logo: logo,
+        //     coinName: name,
+        //     symbols: symbols,
+        // })
+        // coin.save()
+        // .then(coin => {
+        //     return res.json({code: 1, coin: coin})
+        // })
+        // .catch(err => res.json({code: 2, message: err.message}))
+        console.log(req.file);
+        // let file1 = req.files
+        // let name1 = file1.originalname
+        // let newPath1 = path.join(pathUploads, name1)
+        // fs.renameSync(file1.path, newPath1)
+        // fs.rename(file1.path, newPath1, function(err) {
+        //     if ( err ) console.log('ERROR: ' + err);
+        // });
+
+        // let logoCoin = path.join('images', name1)
+        return res.json("OK")
     }
 }
 
--- routes/Coins.js
+++ routes/Coins.js
@@ -1,11 +1,26 @@
 const expres = require('express')
 const router = expres.Router()
 const CoinsController = require('../controllers/CoinsController')
+const multer = require('multer')
+const Path = require('path')
+
+// setting upload
+const pathSave = Path.join(__dirname,'../uploads')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, pathSave)
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({ storage: storage })
 
 // import validator
 
 
+
 // [GET] /add
-router.post('/add', CoinsController.addCoin)
+router.post('/add', upload.single('avatar'), CoinsController.addCoin)
 
 module.exports = router
